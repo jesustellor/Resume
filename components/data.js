43995afb9f4d3f265be90cc7f8deb9d0ef6530a4
jesustellor <jesustellor@gmail.com>
2022-03-09 03:16:26 -0800

    update the README.md with link to contributing guide

---
+++ components/data.js
@@ -0,0 +1,15 @@
+import Resume from "./resume.js";
+
+const myResume = new Resume(
+"resume1",
+"Jesus Tello / Developer",
+"Jesus Tello",
+"../pictures/speedy-avatar.jpg",
+"add here",
+"add 2 here",
+"add 3 here",
+"add 4 here"
+);
+
+const everydayResume = [myResume];
+export default everydayResume;
---
+++ components/resume.js
@@ -0,0 +1,23 @@
+class Resume {
+    constructor(
+        id,
+        title,
+        name,
+        logo,
+        cert1,
+        cert2,
+        cert3,
+        cert4
+    ) {
+        this.id = id;
+        this.title = title;
+        this.name = name;
+        this.logo = logo;
+        this.cert1 = cert1;
+        this.cert2 = cert2;
+        this.cert3 = cert3;
+        this.cert4 = cert4;
+    }
+}
+
+export default Resume;
