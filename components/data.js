19eeef56464c53612e48390580cb4a9b0ec5aba9
jesustellor <jesustellor@gmail.com>
2022-03-09 15:54:33 -0800

    testing object array

--- components/data.js
+++ components/data.js
@@ -11,5 +11,16 @@ const myResume = new Resume(
 "add 4 here"
 );
 
-const everydayResume = [myResume];
+const myResume2 = new Resume(
+    "resume1",
+    "Jesus Tello / Developer",
+    "Jesus Tello",
+    "../pictures/logo.png",
+    "add here",
+    "add 2 here",
+    "add 3 here",
+    "add 4 here"
+    );
+
+const everydayResume = [myResume, myResume2];
 export default everydayResume;
